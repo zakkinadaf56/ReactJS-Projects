062250c72c2fa226a8a08606e58779f2170e475d
zakkinadaf56 <zakkinadaf56@gmail.com>
2022-12-22 12:43:49 +0530

    Refactoring

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -16,64 +16,83 @@ export class News extends Component {
     category:PropTypes.string
   }
 
-  constructor(){
-    super();
+  capitalize=(string)=>{
+    return string.charAt(0).toUpperCase()+string.slice(1);
+  }
+
+  constructor(props){
+    super(props);
    this.state={
     articles: [],
     loading:false,
     page:1
    } 
+   document.title=`${this.capitalize(this.props.category)} - NewsMonkey`;
   }
 
-  async componentDidMount(){
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&pageSize=${this.props.pageSize}`;
+  async updateNews(){
+    const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data=await fetch(url);
     let parsedData=await data.json()
     this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults,loading:false})
   }
 
+
+  async componentDidMount(){
+    // let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&pageSize=${this.props.pageSize}`;
+    // this.setState({loading:true});
+    // let data=await fetch(url);
+    // let parsedData=await data.json()
+    // this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults,loading:false})
+    this.updateNews()
+  }
+
   handlePrevClick = async ()=>{
-    console.log("prev");
+    // console.log("prev");
 
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&page=${this.state.page+-1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
-    let data=await fetch(url);
-    let parsedData=await data.json()
-    this.setState({
-      page:this.state.page-1,
-      articles:parsedData.articles,
-      loading:false
-    })
+    // let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&page=${this.state.page+-1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading:true});
+    // let data=await fetch(url);
+    // let parsedData=await data.json()
+    // this.setState({
+    //   page:this.state.page-1,
+    //   articles:parsedData.articles,
+    //   loading:false
+    // })
+    this.setState({page:this.state.page-1})
+    this.updateNews()
   }
-
-
+  
+  
   handleNextClick = async ()=>{
-    console.log("NExt")
-
-    if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading:true});
-      let data=await fetch(url);
-      let parsedData=await data.json()
-      this.setState({
-        page:this.state.page+1,
-        articles:parsedData.articles,
-        loading:false
-      })
-  }
+    //   console.log("NExt")
+    
+    //   if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+      //     let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdebb8fdead74df7845bbb8db2f7b3b8&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      //     this.setState({loading:true});
+      //     let data=await fetch(url);
+      //     let parsedData=await data.json()
+      //     this.setState({
+        //       page:this.state.page+1,
+        //       articles:parsedData.articles,
+        //       loading:false
+        //     })
+        // }
+        this.setState({page:this.state.page+1})
+        this.updateNews()
 }
 
   render() {
     return (
       <div className='container my-3'>
-        <h1 className="text-center" style={{margin: '35px 0px'}}>NewsMonkey Top headlines</h1>
+        <h1 className="text-center" style={{margin: '35px 0px'}}>NewsMonkey Top headlines on {this.capitalize(this.props.category)}</h1>
         {this.state.loading &&<Spinner/>}
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
 
          return   <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,80):""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
+              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,80):""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
             </div>
         })}
         </div>
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imgUrl,newsUrl,author,date}=this.props;
+    let {title,description,imgUrl,newsUrl,author,date,source}=this.props;
     return (
       <div className='my-3'>
         <div className="card">
+        <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+          {source}
+          <span className="visually-hidden">unread messages</span></span>
           <img src={!imgUrl?"https://images.moneycontrol.com/static-mcnews/2022/12/sensex_nifty_market-770x433.jpg":imgUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}... 
+          </h5>
             <p className="card-text">{description}...</p>
             <p className="card-text"><small className="text-muted">By {!author? "Unknown":author} on {new Date(date).toGMTString()}</small></p>
             <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
