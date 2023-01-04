5934b8a960d2bef03a1a8116a55ef174444df177
zakkinadaf56 <zakkinadaf56@gmail.com>
2023-01-04 14:18:16 +0530

    adding a middleware

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET="ZakkiNadafBro"
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
+const JWT_SECRET = "ZakkiNadafBro";
 
-//Create a user using:POST "/api/auth/createuser"
+//Route 2: Create a user using:POST "/api/auth/createuser"
 
 router.post(
   "/createuser",
@@ -31,23 +32,22 @@ router.post(
           .status(400)
           .json({ error: "Sry user with this email already exist" });
       }
-      const salt=await bcrypt.genSalt(10);
-      const secPass=await bcrypt.hash(req.body.password,salt) 
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       //Create a User
       user = await User.create({
         name: req.body.name,
-        password:secPass ,
+        password: secPass,
         email: req.body.email,
       });
-     const data={
-      user:{
-        id:user.id
-      }
-     }
-     const authToken= jwt.sign(data,JWT_SECRET);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
       // res.json(user);
-      res.json({authToken})
-
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
@@ -55,7 +55,7 @@ router.post(
   }
 );
 
-//Authenticate a User using:POST '/api/auth/login
+//Route 2: Authenticate a User using:POST '/api/auth/login
 router.post(
   "/login",
   [
@@ -69,31 +69,49 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const {email,password}=req.body;
+    const { email, password } = req.body;
     try {
-      let user=await User.findOne({email});
-      if(!user){
-        return res.status(400).json({error:"Please try to login with corrwect credentials"})
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with corrwect credentials" });
       }
-      const passwordCompare=await bcrypt.compare(password,user.password);
-      if(!passwordCompare){
-        return res.status(400).json({error:"Please try to login with corrwect credentials"})  
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with corrwect credentials" });
       }
 
-      const data={
-        user:{
-          id:user.id
-        }
-       }
-       const authToken= jwt.sign(data,JWT_SECRET);
-       res.json({authToken})
-
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
     }
-  
-  })
+  }
+);
+
+//Route 3: Get loged in user details using:POST '/api/auth/getuser
+router.post(
+  "/getuser", fetchuser, async (req, res) => {
 
 
+    try {
+     const userId = req.user.id;
+      // const user = await User.findOne({userId}).select("-password");
+      const user = await User.findById(userId).select("-password");
+      res.send(user)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 module.exports = router;
