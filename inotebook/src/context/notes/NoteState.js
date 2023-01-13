692711d4c69e3458af451cb1b1fa2d004d327529
zakkinadaf56 <zakkinadaf56@gmail.com>
2023-01-13 20:47:49 +0530

    context API

---
+++ inotebook/src/context/notes/NoteState.js
@@ -0,0 +1,12 @@
+import react from "react";
+import noteContext from "./noteContext";
+
+const NoteState= (props)=>{
+    return(
+        <NoteState.provider>
+            {props.children}
+        </NoteState.provider>
+    )
+}
+
+export default NoteState;
